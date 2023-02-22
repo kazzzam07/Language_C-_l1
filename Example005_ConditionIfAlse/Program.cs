12af229576bfa499f119fcfaf0cc9873889c8009
Kazzzam07 <Kazzzam07@gmail.com>
2023-02-22 13:53:18 +0300

    add Example005 Condition If Else

---
+++ Example005_ConditionIfAlse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это жe Маша!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
